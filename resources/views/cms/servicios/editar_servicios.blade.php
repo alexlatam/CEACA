4f1bc73912d7e2afd687c6d70decabfcbfe03bf9
alexis9261 <alexisamm9261@gmail.com>
2020-07-13 23:38:40 -0400

    Update editar_servicios.blade.php

--- resources/views/cms/servicios/editar_servicios.blade.php
+++ resources/views/cms/servicios/editar_servicios.blade.php
@@ -20,14 +20,23 @@
 
       <form action="/cms/actualizar/servicio/{{$servicio->id}}" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="row col-md-6">
+        <div class="row">
           <div class="col-12 mb-4">
             <h5>Titulo</h5>
             <input class="form-control" type="text" name="titulo_servicio" value="{{$servicio->titulo}}" placeholder="Titulo">
           </div>
+          <div class="col-md-6 mb-4">
+            <h5>Categoría</h5>
+            <select name="categoria_servicio" class="form-control form-control">
+              <option>Seleccionar Categoria</option>
+              @foreach($categorias as $categoria)
+              <option value="{{$categoria->id}}" <?php if($servicio->categoria->id == $categoria->id) echo 'selected'?> >{{$categoria->name}}</option>
+              @endforeach
+            </select>
+          </div>
           <div class="col-12 mb-4">
             <h5>Descripción</h5>
-            <textarea class="form-control" name="descripcion_servicio">{{$servicio->descripcion}}</textarea>
+            <textarea class="form-control" name="descripcion_servicio" rows="6">{{$servicio->descripcion}}</textarea>
           </div>
           <div class="col-12 mb-4">
             <h5>Cambiar Imagen</h5>
@@ -37,17 +46,9 @@
             <h5>Cambiar Logo</h5>
             <input type="file" name="logo_servicio">
           </div>
-          <div class="col-12 mb-4">
-            <h5>Categoría</h5>
-            <select name="categoria_servicio" class="form-control form-control">
-              <option>Seleccionar Categoria</option>
-              @foreach($categorias as $categoria)
-              <option value="{{$categoria->id}}" <?php if($servicio->categoria->id == $categoria->id) echo 'selected'?> >{{$categoria->name}}</option>
-              @endforeach
-            </select>
-          </div>
+          
           <div class="col-12 mb-5">
-            <input type="submit" class="btn btn-primary" value="Actualizar Servicio">
+            <input type="submit" class="btn btn-success px-5" value="Actualizar Servicio">
           </div>
         </div>
       </form>
